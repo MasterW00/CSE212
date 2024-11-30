a34f48019838c3abec62e6909de3e0036be11c8e
Jared Farnbach <jaredfarnbach@gmail.com>
2024-11-29 20:49:43 -0700

    complete queueing code

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -11,8 +11,13 @@ public class PriorityQueueTests
     // Defect(s) Found: 
     public void TestPriorityQueue_1()
     {
+        //test that priority person comes first in queue
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("jim", 1);
+        priorityQueue.Enqueue("joe", 3);
+        priorityQueue.Enqueue("jake", 4);
+        priorityQueue.Enqueue("joe", 2);
+        Assert.AreEqual(priorityQueue.Dequeue(), "jake");
     }
 
     [TestMethod]
@@ -21,8 +26,19 @@ public class PriorityQueueTests
     // Defect(s) Found: 
     public void TestPriorityQueue_2()
     {
+        //test order of queue is correct based on priority
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("jim", 1);
+        priorityQueue.Enqueue("joe", 3);
+        priorityQueue.Enqueue("jake", 4);
+        priorityQueue.Enqueue("jared", 2);
+        string[] order = {"jake", "joe", "jared", "jim"};
+        foreach (var item in order)
+        {
+            
+            Assert.AreEqual(item, priorityQueue.Dequeue());
+        }
+
     }
 
     // Add more test cases as needed below.
