ea4a520f30bdb2eced28d5adeb3a55bceff86c3d
Vikijy <83970689+MasterW00@users.noreply.github.com>
2024-12-06 12:51:39 -0700

    Update FeatureCollection.cs
    Looked up some reasons why it wouldn't deserialize and wasn't following the format might work idk

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,112 +1,55 @@
 public class FeatureCollection
 {
-    // TODO Problem 5 - ADD YOUR CODE HERE
-    // Create additional classes as necessary
-//     {
-     public string type {get;set;}
-//   type: "FeatureCollection",
-    public Dictionary<string, MetaData> metadata {get;set;}
-//   metadata: {
-//     ...
-//   },
-    public decimal[] bbox {get;set;}
-//   bbox: [
-//     minimum longitude,
-//     minimum latitude,
-//     minimum depth,
-//     maximum longitude,
-//     maximum latitude,
-//     maximum depth
-//   ],
-    public Dictionary<string, Feat> features;
-//   features: [
-//     {
-//       type: "Feature",
-//       properties: {
-//          ...
-//       },
-    public Dictionary<string, Geometry> geometry {get;set;}
-//       geometry: {
-//            ...
-//       },
-        public string id {get;set;}
-//       id: String
-//     },
-//   ]
-       public class Feat{
-        public string type {get;set;}
-        public Dictionary<string, FeatureProperties> properties {get;set;}
+    public string type { get; set; }
+    public MetaData metadata { get; set; }
+    public decimal[] bbox { get; set; }
+    public List<Feat> features { get; set; }
+
+    public class Feat
+    {
+        public string type { get; set; }
+        public FeatureProperties properties { get; set; }
+        public Geometry geometry { get; set; }
+        public string id { get; set; }
     }
-    public class FeatureProperties{
-//         mag: Decimal,
-//         place: String,
-//         time: Long Integer,
-//         updated: Long Integer,
-//         tz: Integer,
-//         url: String,
-//         detail: String,
-//         felt:Integer,
-//         cdi: Decimal,
-//         mmi: Decimal,
-//         alert: String,
-//         status: String,
-//         tsunami: Integer,
-//         sig:Integer,
-//         net: String,
-//         code: String,
-//         ids: String,
-//         sources: String,
-//         types: String,
-//         nst: Integer,
-//         dmin: Decimal,
-//         rms: Decimal,
-//         gap: Decimal,
-//         magType: String,
-//         type: String
-        public decimal mag {get;set;}
-        public string place {get;set;}
-        public long time {get;set;}
-        public long updated {get;set;}
-        public int tz {get;set;}
-        public string url {get;set;}
-        public string detail {get;set;}
-        public int Felt { get; set; }
-        public decimal Cdi { get; set; }
-        public decimal Mmi { get; set; }
-        public string Alert { get; set; }
-        public string Status { get; set; }
-        public int Tsunami { get; set; }
-        public int Sig { get; set; }
-        public string Net { get; set; }
-        public string Code { get; set; }
-        public string Ids { get; set; }
-        public string Sources { get; set; }
-        public string Types { get; set; }
-        public int Nst { get; set; }
-        public decimal Dmin { get; set; }
-        public decimal Rms { get; set; }
-        public decimal Gap { get; set; }
-        public string MagType { get; set; }
-        public string Type { get; set; }
+
+    public class FeatureProperties
+    {
+        public decimal mag { get; set; }
+        public string place { get; set; }
+        public long time { get; set; }
+        public long updated { get; set; }
+        public int tz { get; set; }
+        public string url { get; set; }
+        public string detail { get; set; }
+        public int? felt { get; set; }
+        public decimal? cdi { get; set; }
+        public decimal? mmi { get; set; }
+        public string alert { get; set; }
+        public string status { get; set; }
+        public int? tsunami { get; set; }
+        public int sig { get; set; }
+        public string net { get; set; }
+        public string code { get; set; }
+        public string ids { get; set; }
+        public string sources { get; set; }
+        public string types { get; set; }
+        public int? nst { get; set; }
+        public decimal? dmin { get; set; }
+        public decimal? rms { get; set; }
+        public decimal? gap { get; set; }
+        public string magType { get; set; }
+        public string type { get; set; }
     }
-    public class Geometry{
-//         type: "Point",
-//         coordinates: [
-//           longitude,
-//           latitude,
-//           depth
-//         ]
-        public string type {get;set;}
-        public decimal[] coordinates {get;set;}
+
+    public class Geometry
+    {
+        public string type { get; set; }
+        public decimal[] coordinates { get; set; }
     }
-}
- public class MetaData{
-//     generated: Long Integer,
-//     url: String,
-//     title: String,
-//     api: String,
-//     count: Integer,
-//     status: Integer
+
+    public class MetaData
+    {
         public long generated { get; set; }
         public string url { get; set; }
         public string title { get; set; }
@@ -114,3 +57,4 @@ public class FeatureCollection
         public int count { get; set; }
         public int status { get; set; }
     }
+}
